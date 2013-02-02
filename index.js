64158abcf86c2601666533f2443e56172a93db33
Xueqiao Xu <xueqiaoxu@gmail.com>
2013-02-02 12:11:32 +0800

    Moved index.js to the root dir

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Class     : require('./src/Class'),
+    Component : require('./src/Component'),
+    Entity    : require('./src/Entity'),
+    System    : require('./src/System'),
+    World     : require('./src/World')
+};
--- src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Class     : require('./Class'),
-    Component : require('./Component'),
-    Entity    : require('./Entity'),
-    System    : require('./System'),
-    World     : require('./World')
-};
